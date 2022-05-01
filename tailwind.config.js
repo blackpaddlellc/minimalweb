682c9e89d5be5105f957a7bf6cdf8b264e7a5558
Kevin Turner <kevin@ksturner.com>
2022-04-30 22:40:29 -0500

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    content: ['src/**/*.{html,ts,tsx,js,jsx}'],
+    theme: {
+        extend: {
+            animation: {
+                'spin-slow': 'spin 20s linear infinite',
+            },
+            keyframes: {},
+        },
+    },
+    plugins: [],
+};
